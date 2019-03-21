66df97fd0f093157d728f4f63b1c90c6eb198fb2
GemaRG <44402600+GemaRG@users.noreply.github.com>
2019-03-21 10:08:35 +0100

    Syncrhonization

--- R/printEDY.R
+++ R/printEDY.R
@@ -1,3 +1,8 @@
 print.EDY <- function(x, ...){
-  
+  cat("EDY object\nContains information about males only from the original ExperessionSet\n$Ry: matrix. Relative expression of chrY genes.\n")
+  cat(dim(x$Ry))
+  cat("\n$EDY:\n$threshold: ")
+  cat(x$threshold)
+  cat("\n$eSet:\n")
+  cat(x$eSet)
 }
