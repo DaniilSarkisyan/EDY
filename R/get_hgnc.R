870418e0ef05d01321ff4ce963ef2abb498a0729
GemaRG <44402600+GemaRG@users.noreply.github.com>
2019-04-26 17:47:47 +0200

    Modifications in get_hgnc
    Falta añadir la libreria de EnsDB a Description!!

--- R/get_hgnc.R
+++ R/get_hgnc.R
@@ -5,70 +5,70 @@
 #' numbers, entrezgenes (NCBI gene IDs) or Ensembl stable IDs.
 #' @title Get hgnc symbol
 #' @param x An ExpressionSet.
-#' @param gene.id A string indicating the type of gene ID that we want to get
-#'   hgnc symbol from. It must be one of \code{genBank}, \code{entrezgene} or
-#'   \code{ensembl}.
-#' @param gene.col A string indicating the name of the column that contains the
-#'   \code{gene.id} in the table of features of the ExpressionSet (accessed by
+#' @param key.type A string indicating the type of gene ID that we want to get
+#'   hgnc symbol from. It must be one of \code{'ENTREZID', 'EXONID',
+#'   'GENEBIOTYPE', 'GENEID', 'GENENAME', 'PROTDOMID', 'PROTEINDOMAINID',
+#'   'PROTEINDOMAINSOURCE', 'PROTEINID', 'SEQNAME', 'SEQSTRAND', 'SYMBOL',
+#'   'TXBIOTYPE', 'TXID', 'TXNAME', 'UNIPROTID' and 'GENBANK'}.
+#' @param key.col A string indicating the name of the column that contains the
+#'   \code{key.type} in the table of features of the ExpressionSet (accessed by
 #'   \code{fData}).
 #' 
 #' @importFrom biomaRt useMart getBM
 #' @import org.Hs.eg.db
+#' @import EnsDb.Hsapiens.v86
 #' @export get_hgnc 
-#' @return A \code{data.frame} containing the previous information in \code{x}
-#'   plus 5 more columns: \code{start_position} (of the gene),
-#'   \code{end_position} (of the gene), \code{chromosome_name},
-#'   \code{hgnc_symbol} and \code{entrezgene}.
+#' @return An \code{ExpressionSet} containing the previous information in \code{x}
+#'   plus the column \code{hgnc_symbol} containing the gene symbols.
 
-get_hgnc <- function(x, gene.id, gene.col, ...){
+get_hgnc <- function(x, key.type, key.col, ...){
   
   fData(x)$id.feature <- featureNames(x)
-  query <- fData(x)[, gene.col]
-  type <- charmatch(tolower(gene.id), c("entrezgene", "genbank", "ensembl"))
-  if (is.na(type) || type==0){
-    stop("Invalid gene.id. Try one from 'entrezgene', 'genbank' or 'ensembl'")
+  query <- fData(x)[, key.col]
+  key.id <- toupper(key.id)
+  key.types <- keytypes(EnsDb.Hsapiens.v86)
+  if (key.id%!in%key.types && key.id!="GENBANK"){
+    stop("Invalid key.id. Allowed choices are: 'ENTREZID', 'EXONID', 'GENEBIOTYPE', 'GENEID', 'GENENAME', 'PROTDOMID', 'PROTEINDOMAINID', 'PROTEINDOMAINSOURCE', 'PROTEINID', 'SEQNAME', 'SEQSTRAND', 'SYMBOL', 'TXBIOTYPE', 'TXID', 'TXNAME', 'UNIPROTID' and 'GENBANK'")
   }
   #Get entrezgene from id
-  else if (type == 2 || type == 3){
-    if (type == 2){
-      list_entrez_id <- as.list(org.Hs.egACCNUM2EG)
-    }
-    else { 
-      list_entrez_id <- as.list(org.Hs.egENSEMBL2EG) 
-    }
-    id_entrezgenes <- list_entrez_id[query]
-    id_entrezgenes <- id_entrezgenes[!is.na(names(id_entrezgenes))]
-  
-    id <- names(id_entrezgenes)
-  
-    entrezgenes <- c()
-    for (i in 1:length(id_entrezgenes)){
-      entrezgenes[i] <- id_entrezgenes[[i]]
-    }
-    
-    matrix_entr_id <- matrix(c(entrezgenes, id), ncol = 2)
-    colnames(matrix_entr_id) <- c("entrezgene", gene.col)
-    
+  else if (key.id%in%key.types){
+    hgnc_symbols <- select(EnsDb.Hsapiens.v86, keys = query, keytype = key.type,
+           columns = c(key.type, "SYMBOL"))
     #Join to fData
-    fData(x) <-  merge(matrix_entr_id, fData(x), by.x = gene.col, by.y = gene.col)
-  } 
-  else if (type == 1) {
-    entrezgenes <- query
-    position <- which(names(fData(x))==gene.col)
-    names(fData(x))[position] <- "entrezgene"
-    gene.col <- "entrezgene"
+    fData(x) <- merge(hgnc_symbols, fData(x), by.x = key.type, by.y = key.col)
+    }
+  else { 
+      list_entrez_id <- as.list(org.Hs.egENSEMBL2EG) 
+  
+      id_entrezgenes <- list_entrez_id[query]
+      id_entrezgenes <- id_entrezgenes[!is.na(names(id_entrezgenes))]
+    
+      id <- names(id_entrezgenes)
+    
+      entrezgenes <- c()
+      for (i in 1:length(id_entrezgenes)){
+        entrezgenes[i] <- id_entrezgenes[[i]]
+      }
+      
+      matrix_entr_id <- matrix(c(entrezgenes, id), ncol = 2)
+      colnames(matrix_entr_id) <- c("entrezgene", key.col)
+      
+      #Join to fData
+      fData(x) <-  merge(matrix_entr_id, fData(x), by.x = key.col, by.y = key.col)
+      
+      # Get hgnc_symbol from entrezgene
+      ensembl <- useMart("ensembl", dataset="hsapiens_gene_ensembl")
+    
+      hgnc_symbols <- getBM(attributes = c("hgnc_symbol", "entrezgene"), 
+                            filters = "entrezgene",
+                            values = entrezgenes, mart = ensembl)
+      
+      #Join to fData
+      fData(x) <- merge(hgnc_symbols, fData(x), by.x = "entrezgene", by.y = "entrezgene")
   }
-  # Get hgnc_symbol from entrezgene
-  ensembl <- useMart("ensembl", dataset="hsapiens_gene_ensembl")
 
-  hgnc_symbols <- getBM(attributes = c("start_position", "end_position", "chromosome_name", 
-                                       "hgnc_symbol", "entrezgene"), filters = "entrezgene",
-                        values = entrezgenes, mart = ensembl)
-
-  #Join to fData
-  fData(x) <- merge(hgnc_symbols, fData(x), by.x = "entrezgene", by.y = "entrezgene")
   #Delete duplicated ids
-  fData(x) <- fData(x)[!duplicated(fData(x)[,gene.col]),]
+  fData(x) <- fData(x)[!duplicated(fData(x)[,key.col]),]
   
   #output
   x
